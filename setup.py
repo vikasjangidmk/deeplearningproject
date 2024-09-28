56635bd9cda588809fd1d51d0b98a2fc96288db2
vikasjangidmk <130329907+vikasjangidmk@users.noreply.github.com>
2024-09-28 11:47:26 +0530

    requirement updated

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import find_packages,setup
+from typing import List
 
+HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)->list[str]:
+def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
-        
+
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-            
-        return requirements
+
+    return requirements
+
 
 setup(
-    
+
 name="Xray",
 version="0.0.1",
 author="Vikas Jangid",
-author_email="vikasjangid@gmail.com",
-install_requires=get_requirements(),
-package=find_packages(),
+author_email="vikasjangidmk@gmail.com",
+install_requires=get_requirements(r"C:\Users\vikas\OneDrive\Desktop\deeplearningproject\requirements_dev.txt"),
+package=find_packages()
+
 )
