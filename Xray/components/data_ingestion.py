99dbfa9e26f6e265bad9984d3f85f30ca3260d3f
vikasjangidmk <130329907+vikasjangidmk@users.noreply.github.com>
2024-09-28 12:13:37 +0530

    files updated

--- Xray/components/data_ingestion.py
+++ Xray/components/data_ingestion.py
@@ -6,3 +6,19 @@ from Xray.entity.artifact_entity import DataIngestionArtifact
 from Xray.entity.config_entity import DataIngestionConfig
 from Xray.exception import XRayException
 from Xray.logger import logging
+
+
+class DataIngestion:
+    def __init__(self):
+        pass
+    def get_data_from_s3(self):
+        try:
+            pass
+        except Exception as e:
+            raise XRayException(e,sys)
+        
+    def initiate_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            raise XRayException(e,sys)
